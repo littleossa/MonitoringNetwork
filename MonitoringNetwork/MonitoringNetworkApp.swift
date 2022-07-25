f66102357b26819b02070576968957aab6d88f05
littleossa <littleossa@gmail.com>
2022-07-25 17:27:37 +0900

    Initial Commit

---
+++ MonitoringNetwork/MonitoringNetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonitoringNetworkApp.swift
+//  MonitoringNetwork
+//
+//  Created by 平岡修 on 2022/07/25.
+//
+
+import SwiftUI
+
+@main
+struct MonitoringNetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
